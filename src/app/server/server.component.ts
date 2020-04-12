9480d57753000c60652429f0ace7bb5834fabb81
ashishchopra24 <49956840+ashishchopra24@users.noreply.github.com>
2020-04-12 15:18:59 +0530

    StringInterpolation

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -7,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServerComponent implements OnInit {
 
+serverId = 100;
+serverStatus = 'online';
+
+
   constructor() { }
 
   ngOnInit() {
