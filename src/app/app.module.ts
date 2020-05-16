0c8c4a6616bc8e829021c83ebd60c80d3de8ebc1
ashishchopra24 <49956840+ashishchopra24@users.noreply.github.com>
2020-05-16 12:37:44 +0530

    NewOne2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ServersComponent } from './servers/servers.component';
 import {FormsModule} from '@angular/forms';
-import {ServerElementComponent} from './servers/serverElement/serverelement.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServersComponent,
-    ServerElementComponent
+    ServersComponent
   ],
   imports: [
     BrowserModule,
